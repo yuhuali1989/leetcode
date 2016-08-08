c0ddfe87f2dde2a5bf6edf32223e4c9085d20aa0
yuhuali <yuhuali@smartdevices.com.cn>
2016-08-08 11:14:34 +0800

    add maven support

--- src/test/com/huali/leetcode/test/testReverseString.java
+++ src/test/com/huali/leetcode/test/testReverseString.java
@@ -1,7 +1,16 @@
 package com.huali.leetcode.test;
 
+
+import org.junit.Test;
+
 /**
  * Created by yuhuali on 16/8/7.
  */
 public class testReverseString {
+    @Test
+    public void reverseStringTest(){
+
+    }
+
+
 }
